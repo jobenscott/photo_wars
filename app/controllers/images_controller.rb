5e3371fd27a835a6f7edc449a9f4af1072951975
jobenscott <your.jobenscott@yahoo.com>
2015-03-21 11:12:43 -0600

    ruben work on paperclip commit

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,10 +1,54 @@
 class ImagesController < ApplicationController
+  
   def index
+    @images = Image.all.order(vote: :desc)
   end
 
   def profile
+    @images = current_user.Image
   end
 
   def show
+    @image = Image.find(params[:id])
   end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+
+    if @image.save
+      redirect_to image_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @image = Image.find(params[:id])
+  end
+
+  def update
+    @image = Image.find(params[:id])
+
+    if @image.update(image_params)
+      redirect_to image_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+    def image_params
+      params.require(:image).permit(:title, :url, :vote, :description, :user_id)
+    end
 end
