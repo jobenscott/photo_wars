0cee27503e9c5c948acd608008ef9453a4c0e858
Ruben Sanchez <rfv3@yahoo.com>
2015-03-21 13:00:46 -0600

    added paperclip and config

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,6 @@
 class Image < ActiveRecord::Base
+
+  has_attached_file :url, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :url, :content_type => /\Aimage\/.*\Z/
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150321172641) do
+ActiveRecord::Schema.define(version: 20150321185714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,16 @@ ActiveRecord::Schema.define(version: 20150321172641) do
     t.integer  "vote"
     t.text     "description"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
+    t.string   "url_file_name"
+    t.string   "url_content_type"
+    t.integer  "url_file_size"
+    t.datetime "url_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
